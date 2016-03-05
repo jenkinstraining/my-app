4562a53a6db66207221dbaf961aec6123f1de6de
jenkins_training <chef.opscode.training@gmail.com>
2016-03-05 12:05:54 +0530

    push the chnages for poll scm

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World testing" );
+
+        System.out.println( "Hello World testing, chnage for poll scm." );
     }
 }
