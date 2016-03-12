0a9d5bfbc3b008322b46a4075634efc21da03f49
jenkins_training <chef.opscode.training@gmail.com>
2016-03-12 23:53:03 +0530

    git fix

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,11 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-<<<<<<< HEAD
 
         System.out.println( "Hello World testing, chnage for poll scm." );
-=======
         System.out.println( "Hello World testing" );
->>>>>>> 428214a751f2b5babe77baf3c70658f69484e774
     }
 }
